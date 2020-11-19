7c6e0f16be4651d6e1815edede5768547bb45a6e
Laurens Rouw <lrouw@cfp.nl>
2020-11-19 12:50:23 +0100

    Add node-sass

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Board.module.css";
+import styles from "./Board.module.scss";
 
 function Board() {
   return <div className={styles.board}>Quicks board</div>;
