63ad1d6cb1ad199573aae4f98fa73f0d9357a01d
Laurens Rouw <lrouw@cfp.nl>
2020-11-19 17:33:46 +0100

    Add dynamic numbers

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -4,13 +4,16 @@ import styles from "./Board.module.scss";
 import Row from "./Row";
 import ScoreInfo from "./ScoreInfo";
 
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const reversed = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
 function Board() {
   return (
     <div className={styles.board}>
-      <Row color="RED" />
-      <Row color="YELLOW" />
-      <Row color="GREEN" />
-      <Row color="BLUE" />
+      <Row color="RED" numbers={numbers} />
+      <Row color="YELLOW" numbers={numbers} />
+      <Row color="GREEN" numbers={reversed} />
+      <Row color="BLUE" numbers={reversed} />
       <ScoreInfo />
     </div>
   );
--- src/components/board/Row.tsx
+++ src/components/board/Row.tsx
@@ -8,6 +8,7 @@ import Lock from "./Lock";
 
 interface Props {
   color: DiceColor;
+  numbers: number[];
 }
 
 function Row(props: Props) {
@@ -16,17 +17,9 @@ function Row(props: Props) {
   return (
     <div className={style}>
       <div className={styles.inner}>
-        <Box color={props.color}>2</Box>
-        <Box color={props.color}>3</Box>
-        <Box color={props.color}>4</Box>
-        <Box color={props.color}>5</Box>
-        <Box color={props.color}>6</Box>
-        <Box color={props.color}>7</Box>
-        <Box color={props.color}>8</Box>
-        <Box color={props.color}>9</Box>
-        <Box color={props.color}>10</Box>
-        <Box color={props.color}>11</Box>
-        <Box color={props.color}>12</Box>
+        {props.numbers.map((n) => (
+          <Box color={props.color}>{n}</Box>
+        ))}
       </div>
       <div className={styles.spacer} />
       <Lock color={props.color} />
