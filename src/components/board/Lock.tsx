65befdebf113e7b897f000e5de4fa66c4474699f
Laurens Rouw <lrouw@cfp.nl>
2020-11-20 20:59:31 +0100

    Add bonus logic

--- src/components/board/Lock.tsx
+++ src/components/board/Lock.tsx
@@ -1,34 +1,41 @@
 import React from "react";
 import styles from "./Lock.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLockOpen } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
 
 import { DiceColor } from "../../model/colors";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
 interface Props {
   color: DiceColor;
 }
 
 function Lock(props: Props) {
-  const style = colorsToStyle(props.color);
+  const checked = useSelector((state: RootState) => state[props.color].checked);
+
+  const count = checked.reduce((prev, cur) => (cur ? prev + 1 : prev), 0);
+  const bonus = count >= 5 && checked[10];
+
+  const style = colorsToStyle(props.color, bonus);
 
   return (
     <div className={style}>
-      <FontAwesomeIcon icon={faLockOpen} />
+      <FontAwesomeIcon icon={bonus ? faLockOpen : faLock} />
     </div>
   );
 }
 
-function colorsToStyle(color: DiceColor) {
+function colorsToStyle(color: DiceColor, bonus: boolean) {
   switch (color) {
     case "RED":
-      return styles.red;
+      return bonus ? styles.redChecked : styles.red;
     case "YELLOW":
-      return styles.yellow;
+      return bonus ? styles.yellowChecked : styles.yellow;
     case "GREEN":
-      return styles.green;
+      return bonus ? styles.greenChecked : styles.green;
     case "BLUE":
-      return styles.blue;
+      return bonus ? styles.blueChecked : styles.blue;
   }
 }
 
